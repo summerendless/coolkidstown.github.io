40e61f8b367c370808ff6f6af13b573f64969bcd
summerendless <46341018+summerendless@users.noreply.github.com>
2021-05-30 02:11:51 +0900

    .merge

---
+++ js/cool.js
@@ -0,0 +1,24 @@
+
+const bgm = "#bgm";
+
+$( document ).ready(function() {
+    $(bgm).volume = 0.8;
+});
+
+//이미지(png)로 다운로드
+function downImg(){
+    html2canvas($(".bg-wrap")[0]).then(function(canvas){
+        var myImage = canvas.toDataURL();
+        downloadURI(myImage, "저장할 파일명.png") 
+    });
+}
+
+function downloadURI(uri, name){
+    var link = document.createElement("a")
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+}
+
+$( '.bg-wrap .word' ).draggable();
