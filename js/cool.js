9fe7728c488ffbda74fe2062c16b91d283fb98cb
summerendless <46341018+summerendless@users.noreply.github.com>
2021-05-30 10:05:01 +0900

    fixing js

--- js/cool.js
+++ js/cool.js
@@ -1,18 +1,3 @@
 
-//이미지(png)로 다운로드
-function downImg(){
-    html2canvas($(".bg-wrap")[0]).then(function(canvas){
-        var myImage = canvas.toDataURL();
-        downloadURI(myImage, "저장할 파일명.png") 
-    });
-}
-
-function downloadURI(uri, name){
-    var link = document.createElement("a")
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-}
 
 $( '.bg-wrap .word' ).draggable();
